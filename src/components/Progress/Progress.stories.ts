28abca1643ad3cb7b2213e2060c6918c4d7201df
Dana Ciocan <danaciocan@economist.com>
2021-09-05 21:46:27 +0100

    Make progress bar component dumb

--- src/components/Progress/Progress.stories.ts
+++ src/components/Progress/Progress.stories.ts
@@ -1,16 +1,14 @@
 import Progress from './Progress.vue';
 
 type Arguments = {
-  currentAcronym: number;
-  totalAcronyms: number;
+  percentage: number;
 };
 
 export default {
   title: 'Progress',
   component: Progress,
   argTypes: {
-    currentAcronym: Number,
-    totalAcronyms: Number,
+    percentage: Number,
   },
 };
 
@@ -22,7 +20,7 @@ const Template = (args: Arguments) => ({
   template: '<Progress v-bind="args" />',
 });
 
-export const Primary = Template.bind({ currentAcronym: 0, totalAcronyms: 10 });
+export const Primary = Template.bind({ percentage: 50 });
 
 // @ts-ignore Property does not exist (2339)
 Primary.storyName = 'Progress';
--- src/components/Progress/Progress.vue
+++ src/components/Progress/Progress.vue
@@ -2,8 +2,9 @@
   <div class="progress">
     <div class="progress-container">
       <div
+        v-for="n in percentage"
+        :key="`progress-${n}`"
         class="progress-bar"
-        :class="getProgressClass(currentAcronym, totalAcronyms)"
       ></div>
     </div>
   </div>
@@ -15,14 +16,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'Progress',
   props: {
-    currentAcronym: Number,
-    totalAcronyms: Number,
-  },
-  methods: {
-    getProgressClass: (current: number, total: number) => {
-      const progressPercent: number = Math.round((current / total) * 100);
-      return `progress-${Math.round(progressPercent / 10) * 10}`;
-    },
+    percentage: Number,
   },
 });
 </script>
@@ -39,44 +33,14 @@ export default defineComponent({
   border: 1px solid #333;
   height: 1.5rem;
   padding: 0.25rem;
+  display: flex;
+  flex-direction: row;
 }
 
 .progress-bar {
   background-color: green;
   height: 1.5rem;
-}
-
-.progress-bar.progress-0 {
-  width: 0%;
-}
-.progress-bar.progress-10 {
-  width: 10%;
-}
-.progress-bar.progress-20 {
-  width: 20%;
-}
-.progress-bar.progress-30 {
-  width: 30%;
-}
-.progress-bar.progress-40 {
-  width: 40%;
-}
-.progress-bar.progress-50 {
-  width: 50%;
-}
-.progress-bar.progress-60 {
-  width: 60%;
-}
-.progress-bar.progress-70 {
-  width: 70%;
-}
-.progress-bar.progress-80 {
-  width: 80%;
-}
-.progress-bar.progress-90 {
-  width: 90%;
-}
-.progress-bar.progress-100 {
-  width: 100%;
+  width: 1%;
+  justify-content: flex-start;
 }
 </style>
