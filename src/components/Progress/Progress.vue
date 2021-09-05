2d3d6640b13b1d1a09c65d91a7e0e8eda335598a
Dana Ciocan <danaciocan@economist.com>
2021-09-05 21:35:47 +0100

    Rewrite styles so they're mobile first

--- src/components/Progress/Progress.vue
+++ src/components/Progress/Progress.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="progress-container">
-    <div
-      class="progress"
-      :class="getProgressClass(currentAcronym, totalAcronyms)"
-    ></div>
+  <div class="progress">
+    <div class="progress-container">
+      <div
+        class="progress-bar"
+        :class="getProgressClass(currentAcronym, totalAcronyms)"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -26,60 +28,52 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.progress-container {
-  border: 1px solid black;
-  height: 15px;
-  padding: 3px;
-  width: 50%;
-  margin: 15px auto;
-}
-
 .progress {
+  box-sizing: border-box;
+  width: 100%;
+  height: 4rem;
+  padding: 1rem;
+}
+
+.progress-container {
+  border: 1px solid #333;
+  height: 1.5rem;
+  padding: 0.25rem;
+}
+
+.progress-bar {
   background-color: green;
-  height: 15px;
+  height: 1.5rem;
 }
 
-.progress-0 {
-  width: 0;
-}
-
-.progress-10 {
+.progress-bar.progress-10 {
   width: 10%;
 }
-
-.progress-20 {
+.progress-bar.progress-20 {
   width: 20%;
 }
-
-.progress-30 {
+.progress-bar.progress-30 {
   width: 30%;
 }
-
-.progress-40 {
+.progress-bar.progress-40 {
   width: 40%;
 }
-
-.progress-50 {
+.progress-bar.progress-50 {
   width: 50%;
 }
-
-.progress-60 {
+.progress-bar.progress-60 {
   width: 60%;
 }
-
-.progress-70 {
+.progress-bar.progress-70 {
   width: 70%;
 }
-
-.progress-80 {
+.progress-bar.progress-80 {
   width: 80%;
 }
-
-.progress-90 {
+.progress-bar.progress-90 {
   width: 90%;
 }
-
-.progress-100 {
+.progress-bar.progress-100 {
   width: 100%;
 }
 </style>
